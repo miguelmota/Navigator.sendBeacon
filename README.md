640c80f988fce796414e57c883739aaa67850930
Miguel Mota <miguelmota2@gmail.com>
2015-04-21 21:50:20 -0700

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# sendbeacon-polyfill
+
+Polyfill for [Navigator.sendBeacon()](http://www.w3.org/TR/beacon/#sec-sendBeacon-method)
+
+[![NPM](https://nodei.co/npm/sendbeacon-polyfill.png)](https://nodei.co/npm/sendbeacon-polyfill)
+
+# Install
+
+```bash
+npm install sendbeacon-polyfill
+```
+
+```bash
+bower install sendbeacon-polyfill
+```
+
+# Usage
+
+Read the [MDN Navigator.sendBeacon() docs](https://developer.mozilla.org/en-US/docs/Web/API/Navigator/sendBeacon).
+
+# License
+
+MIT
---
+++ sendbeacon.js
@@ -0,0 +1,21 @@
+(function(root) {
+  'use strict';
+
+  if (!('sendBeacon' in navigator)) {
+    navigator.sendBeacon1 = function(url, data) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', url, false);
+      xhr.setRequestHeader('Accept', '*/*');
+      if (typeof data === 'string') {
+        xhr.setRequestHeader('Content-Type', 'text/plain;charset=UTF-8');
+        xhr.responseType = 'text/plain';
+      } else if (Object.prototype.toString.call(data) === '[object Blob]') {
+        if (data.type) {
+          xhr.setRequestHeader('Content-Type', data.type);
+        }
+      }
+      xhr.send(data);
+      return true;
+    };
+  }
+})(this);
