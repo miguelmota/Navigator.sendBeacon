bae8b7ed79ea4f6492ab49f9300937d6a4c4e538
Miguel Mota <miguelmota2@gmail.com>
2017-08-14 11:35:35 -0700

    remove bower + timeout that doesn't work

--- sendbeacon.js
+++ sendbeacon.js
@@ -1,7 +1,7 @@
 (function(root) {
   'use strict';
 
-  function sendBeacon(url, data, _settings) {
+  function sendBeacon(url, data) {
     var xhr = ('XMLHttpRequest' in window) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
     xhr.open('POST', url, false);
     xhr.withCredentials = true;
@@ -12,18 +12,6 @@
     } else if (Object.prototype.toString.call(data) === '[object Blob]') {
       if (data.type) {
         xhr.setRequestHeader('Content-Type', data.type);
-      }
-    }
-
-    if (typeof _settings === 'object') {
-      if (!isNaN(_settings.timeout)) {
-        // xhr.timeout doesn't work for synchronous requests
-        setTimeout(function() {
-          if (xhr.readyState !== 4) {
-            xhr.abort();
-          }
-        }, _settings.timeout);
-        ;
       }
     }
 
