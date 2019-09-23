cd5305f5cbe86c6365e2e47d80b21d345d5b9068
Miguel Mota <miguelmota2@gmail.com>
2019-09-22 21:01:44 -0700

    Add linter

--- sendbeacon.js
+++ sendbeacon.js
@@ -1,41 +1,40 @@
-const isString = val => typeof val === 'string';
-const isBlob = val => val instanceof Blob;
+const isString = val => typeof val === 'string'
+const isBlob = val => val instanceof Blob
 
-polyfill.call(typeof window === 'object' ? window : this || {});
+polyfill.call(typeof window === 'object' ? window : this || {})
 
-function polyfill() {
-  if (isSupported.call(this)) return;
+function polyfill () {
+  if (isSupported.call(this)) return
 
-  if (!('navigator' in this)) this.navigator = {};
-  this.navigator.sendBeacon = sendBeacon.bind(this);
-};
+  if (!('navigator' in this)) this.navigator = {}
+  this.navigator.sendBeacon = sendBeacon.bind(this)
+}
 
-function sendBeacon(url, data) {
-  const event = this.event && this.event.type;
-  const sync = event === 'unload' || event === 'beforeunload';
+function sendBeacon (url, data) {
+  const event = this.event && this.event.type
+  const sync = event === 'unload' || event === 'beforeunload'
 
-  const xhr = ('XMLHttpRequest' in this) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-  xhr.open('POST', url, !sync);
-  xhr.withCredentials = true;
-  xhr.setRequestHeader('Accept', '*/*');
-
+  const xhr = ('XMLHttpRequest' in this) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP')
+  xhr.open('POST', url, !sync)
+  xhr.withCredentials = true
+  xhr.setRequestHeader('Accept', '*/*')
 
   if (isString(data)) {
-    xhr.setRequestHeader('Content-Type', 'text/plain;charset=UTF-8');
-    xhr.responseType = 'text/plain';
+    xhr.setRequestHeader('Content-Type', 'text/plain;charset=UTF-8')
+    xhr.responseType = 'text/plain'
   } else if (isBlob(data) && data.type) {
-    xhr.setRequestHeader('Content-Type', data.type);
+    xhr.setRequestHeader('Content-Type', data.type)
   }
 
   try {
-    xhr.send(data);
+    xhr.send(data)
   } catch (error) {
-    return false;
+    return false
   }
 
-  return true;
+  return true
 }
 
-function isSupported() {
-  return ('navigator' in this) && ('sendBeacon' in this.navigator);
+function isSupported () {
+  return ('navigator' in this) && ('sendBeacon' in this.navigator)
 }
